3eeb3e282b742491102f936d6b57b017768f9161
jasurbekmaxamadaliyev <jasurbekmaxamadaliyev777@gmail.com>
2025-05-22 16:35:48 +0500

    switch and loop

---
+++ 5-dars loop/index1.js
@@ -0,0 +1,49 @@
+// console.log(null ?? undefined ?? 1);
+// amal = "-";
+// switch (amal) {
+//   case "-":
+//     console.log("-");
+
+//     break;
+//   case "+":
+//     console.log("+");
+
+//   default:
+//     console.log("bu shart notogri");
+
+//     break;
+// }
+// while (i >= 1) {
+//   res *= i; // res = res * i / 5 * 4 *3 *2*1=120
+//   i--;
+// }
+
+// console.log(res);
+// let res = 1;
+
+// do {
+//   res += i;
+//   i--;
+// } while (i >= 1);
+
+// console.log(res);
+
+// let i = 10;
+// for (i; i >= 0; i++) {
+//   let res = 0;
+//   res += i;
+//   console.log(i);
+// }
+// let juft = 0;
+// for (i = 1; i <= 10; i++) {
+//   if (i % 2 == 0) juft += i; // juft = juft +i 2 4 6 8 10
+// }
+// console.log(juft);
+
+// let toq = 0;
+// for (t = 1; t <= 10; t++) {
+//   if (t % 2 !== 0) toq += t; // 1 3 5 7 9
+// }
+// console.log(toq);
+
+// console.log(5555);
---
+++ 5-dars loop/node.md
@@ -0,0 +1,36 @@
+# Nullish
+
+?? -- bu nullish belgisi
+nullish da null va undefined false qiymat boladi qolgan qiymatlar true
+
+# Switch case
+
+if bilan switch bir biriga oxshash
+if da biz shart tekshiramiz , bizga keladigan malumot 100% qanday bolishini bilsak shunda switch ishlatamiz
+if da biz condition yozamiz switchda esa variable yozamiz
+switch syntax:
+switch(variable){
+case:
+}
+break ---- agar case togri bolsa keyingilarini oqimaydi
+
+# Loop
+
+    Loop --- while,do while,for
+
+While syntax :
+while(condition){
+code
+}
+
+do while syntax:
+do{
+code
+}while(condition)
+
+while VS do While
+
+for syntax:
+for(variable,condition,step){
+code
+}
